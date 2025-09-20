288e240ff4e0b3ee129e202862f7c586a90cceaf
MartinMontagner <martinmontagner@estudiante.mdp.utn.edu.ar>
2025-09-20 12:10:16 -0300

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GYMUNQUmDL3OnNRTpsKphjP96YlnF6MmHQak4jgyivM=\"\n}"
---
+++ app/page.js
@@ -0,0 +1,8 @@
+import MainHeader from '@/components/mainHeader/main-header';
+
+export default function HomePage() {
+  return (
+    <h1>Next.js Routing & Page Rendering</h1>
+  );
+}
+  
