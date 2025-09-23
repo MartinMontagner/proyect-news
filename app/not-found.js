318be75918c01097a9c0f94e7a23b67263e358e4
MartinMontagner <martinmontagner@estudiante.mdp.utn.edu.ar>
2025-09-23 09:03:28 -0300

    not found slug page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div id="error">
+      <h1 >Pagina no encontrada</h1>
+      <p >Por favor ingrese una pagina existente</p>
+    </div>
+  );
+}
